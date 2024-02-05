fc166f7d4acb2fe1286aab475631921264d4b17f
mohammadaltamas <48707190+mohammadaltamas@users.noreply.github.com>
2024-02-05 11:59:26 +0530

    check in code for the changes the Loading, button, add app icon
    check in code for the changes the Loading, button, add app icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { InfoCircleFilled } from '@ant-design/icons';
 import * as React from 'react';
 import { createRef, useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import loaderGif from './loader.gif';
 
 export const defaultSettings = {
   paySecret: '',
@@ -14,41 +12,17 @@ export const defaultSettings = {
   options: {
     applePay: {
       enabled: true,
-    },
-
+    }
   },
 };
 
 const tyroJSRef = createRef();
 
-const loaderStyle = {
-  display: 'flex',
-  width: '100%',
-  height: '100%',
-  backgroundColor: '#f5f6f7',
-  opacity: 0.8,
-  position: 'fixed',
-  zIndex: 99,
-  justifyContent: 'center',
-  alignItems: 'center'
-};
-
-const buttonStyle = {
-  borderRadius: '5px',
-  width: '100%',
-  backgroundColor: '#9E8CCC',
-  border: '0px',
-  color: 'white',
-  padding: '10px',
-  marginTop: '10px',
-  fontSize: '20px',
-};
-
 const App = () => {
   const [configuration, setConfiguration] = useState(defaultSettings);
 
   const [error, setError] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [libraryReady, setLibraryReady] = useState(false);
   const [fetchingPaySecret, setFetchingPaySecret] = useState(false);
   const [payRequestReady, setPayRequestReady] = useState(false);
@@ -56,11 +30,23 @@ const App = () => {
   const [submittingOverlay, setSubmittingOverlay] = useState(false);
   const [submitting, setSubmitting] = useState(false);
   const [payComplete, setPayComplete] = useState(false);
+  const [buttonStyle, setButtonStyle] = useState({
+    borderRadius: '5px',
+    width: '100%',
+    backgroundColor: '#F3C910',
+    border: '0px',
+    color: 'white',
+    padding: '10px',
+    marginTop: '10px',
+    fontSize: '20px',
+  });
 
   const { paySecret, theme, styleProps, options } = configuration;
   let search = window.location.search;
   let params = new URLSearchParams(search);
   let paySecretValue = params.get('paySecretValue');
+  let buttonBorderRadius = params.get('buttonBorderRadius');
+  let buttonBackgroundColor = params.get('buttonBackgroundColor');
 
   // STEP 1, Attach TyroJS Library
   // Installs the TyroJS Library onto the DOM
@@ -72,7 +58,26 @@ const App = () => {
     script.async = true;
     script.onload = () => setLibraryReady(true);
     document.body.appendChild(script);
+
+     if (buttonBorderRadius) {
+      setButtonStyle(prevStyle => ({
+        ...prevStyle,
+        borderRadius: buttonBorderRadius + 'px'
+      }));
+    }
+
+    // Update buttonStyle if buttonBackgroundColor exists
+    if (buttonBackgroundColor) {
+      setButtonStyle(prevStyle => ({
+        ...prevStyle,
+        backgroundColor: '#' + buttonBackgroundColor
+      }));
+    }
+
+    setLoading(true);
   }, []);
+
+  
 
   // STEP 2, Fetch the Pay Secret
   useEffect(() => {
@@ -202,9 +207,6 @@ const App = () => {
     const payRequest = await tyroJSRef.current.tyroJSInstance.fetchPayRequest();
     // display result
     if (payRequest.status === 'SUCCESS') {
-      setPayComplete(payRequest);
-      setSubmitting(false);
-      setLoading(false);
       window.ReactNativeWebView.postMessage(true);
     } else {
       toast.error(error.toString())
@@ -222,29 +224,28 @@ const App = () => {
             <p>({error.errorCode ?? 'UNKNOWN_ERROR'}) {error.message}</p>
           </div>
         ) : null}
-        {payComplete ? (
-          <>Payment has been completed.</>
-        ) : (
-          <>
-            <form id="pay-form">
-              <div id="pay-form-submitting-overlay" style={{ display: submittingOverlay ? 'block' : 'none', position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(255,255,255,0.5)' }}>
-                ... Submitting ...
-              </div>
-              <div id="tyro-pay-form" style={{ visibility: payRequestReady ? 'visible' : 'hidden' }}></div>
-              {/* <LoadingButton type='submit' disabled={submitting} onClick={submitPayForm} loading={submitting} variant="contained" loadingPosition="start" style={{ visibility: payRequestReady ? 'visible' : 'hidden', borderRadius: '10px', width: '100%', backgroundColor: '#9E8CCC', border: '0px', color: 'white', padding: '10px', marginTop: '10px' }}  >Enter card details</LoadingButton> */}
-              <button id="pay-form-submit" onClick={submitPayForm} style={{
-                ...buttonStyle,
-                visibility: payRequestReady ? 'visible' : 'hidden'
-              }} disabled={submitting}>Add Card</button>
-            </form>
-          </>
-        )}
-
+        <>
+          <form id="pay-form">
+            <div id="pay-form-submitting-overlay" style={{ display: submittingOverlay ? 'block' : 'none', position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(255,255,255,0.5)' }}>
+              ... Submitting ...
+            </div>
+            <div id="tyro-pay-form" style={{ visibility: payRequestReady ? 'visible' : 'hidden' }}></div>
+            {/* <LoadingButton type='submit' disabled={submitting} onClick={submitPayForm} loading={submitting} variant="contained" loadingPosition="start" style={{ visibility: payRequestReady ? 'visible' : 'hidden', borderRadius: '10px', width: '100%', backgroundColor: '#9E8CCC', border: '0px', color: 'white', padding: '10px', marginTop: '10px' }}  >Enter card details</LoadingButton> */}
+            <button id="pay-form-submit" onClick={submitPayForm} style={{
+              ...buttonStyle,
+              visibility: payRequestReady ? 'visible' : 'hidden'
+            }} disabled={submitting}>Add Card</button>
+          </form>
+        </>
       </div>
       <ToastContainer />
-      {loading ? <div style={loaderStyle}>
-        <img src={loaderGif} alt='loader' />
-      </div> : ''
+      {loading
+        ? <div className='flex fixed inset-0 items-center justify-center bg-opacity-80 bg-white'>
+          <div style={{ color: `#${buttonBackgroundColor}` }} className="animate-spin inline-block w-8 h-8 border-[3px] border-current border-t-transparent rounded-full dark:text-blue-500" role="status" aria-label="loading">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+        : null
       }
     </div>
   );
